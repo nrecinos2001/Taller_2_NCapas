1a32d7427aa41a8621bb78ad927b28b21aed95e0
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-05-03 23:27:40 -0600

    :bug: Update endpoint

--- src/main/java/com/example/demo/controllers/AuthController.java
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -1,11 +1,13 @@
-package controllers;
+package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthController {
 	
 @GetMapping("/signin")
--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,16 +1,11 @@
-package controllers;
+package com.example.demo.controllers;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,28 +15,28 @@ import com.example.demo.apirest_models.UserDto;
 import com.example.demo.apirest_models.UserEntity;
 import com.example.demo.apirest_models.UsersDto;
 import com.example.demo.services.UserService;
+import com.example.demo.services.implementations.UserServiceImpl;
 
 @RestController
+@RequestMapping("/user")
 @CrossOrigin("*")
 public class UserController {
-	
-	
 	@Autowired
-	private UserService userService;
+	private UserServiceImpl userService;
 	
-	@RequestMapping(value = "/user/all", method = RequestMethod.GET)
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
 		public ResponseEntity<UsersDto> getAll() {
 		List<UsersDto> users = userService.getAll();
 		return new ResponseEntity(users, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	@RequestMapping(value = "", method = RequestMethod.GET)
 	public ResponseEntity<UsersDto> getOneById(@RequestParam(value = "code") String code){
 		UsersDto user = userService.getOneById(code);
 		return new ResponseEntity(user, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/user/", method = RequestMethod.POST)
+	/*@RequestMapping(value = "/user/", method = RequestMethod.POST)
 	public ResponseEntity<UsersDto> create(@Valid @RequestBody UsersDto user){
 		UsersDto userCreated = userService.create(user);
 		return new ResponseEntity(userm HttpStatus.CREATED)
@@ -57,6 +52,6 @@ public class UserController {
 	
 	
 	
-	
+	*/
 
 }
