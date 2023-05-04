351e0ba32722e2a8833d0bcbea5e3b2b80a93d32
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-05-03 20:02:02 -0600

    :construction: Add lis to service

--- src/main/java/com/example/demo/services/implementations/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/implementations/UserServiceImpl.java
@@ -1,19 +1,33 @@
 package com.example.demo.services.implementations;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.example.demo.apirest_models.UserEntity;
 import com.example.demo.services.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
-
+	private static List<UserEntity> users = new ArrayList<UserEntity>();
+	
+	static {
+        users.add(new UserEntity("AH000011", "pass", "Arturo", "Hernandez", "user", true, new Date()));
+        users.add(new UserEntity("AO000022", "pass", "Alejandro", "Ortega", "user", false, new Date()));
+        users.add(new UserEntity("JP000033", "pass", "Josue", "Perez", "user", true, new Date()));
+        users.add(new UserEntity("NR000044", "pass", "Nestor", "Recinos", "admin", true, new Date()));
+    }
+	
 	@Override
-	public String singIn() {
+	public String signIn() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<String> getAll() {
+	public List<UserEntity> getAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
