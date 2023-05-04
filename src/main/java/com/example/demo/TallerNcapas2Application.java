bc1806bca06e12514321cc46619be230f0adef68
Josuepp <62664664+Josuepp@users.noreply.github.com>
2023-05-03 18:44:39 -0600

    Proyect Init

---
+++ src/main/java/com/example/demo/TallerNcapas2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerNcapas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerNcapas2Application.class, args);
+	}
+
+}
