52635024aad408e8f8957bb99815fa4b88294272
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-05-03 19:14:25 -0600

    :construction: Implement interface

--- src/main/java/com/example/demo/services/implementations/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/implementations/UserServiceImpl.java
@@ -1,5 +1,51 @@
 package com.example.demo.services.implementations;
 
-public class UserServiceImpl {
+import java.util.List;
+
+import com.example.demo.services.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public String singIn() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOneById(String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String create(String info) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String changePassword(String info) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toggleActive(String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(String info) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
