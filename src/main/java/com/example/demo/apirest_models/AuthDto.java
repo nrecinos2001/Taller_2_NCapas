3aa5a13bb906ff62fc7c2838cbdc6555c2216c2e
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-05-08 12:07:30 -0600

    :bug: Solve SignUp process

--- src/main/java/com/example/demo/apirest_models/AuthDto.java
+++ src/main/java/com/example/demo/apirest_models/AuthDto.java
@@ -2,9 +2,10 @@ package com.example.demo.apirest_models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class AuthDto {
 	private String identifier;
 	private String password;
